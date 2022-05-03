ed8ee64fe6478eb26f21bb63c1265a928cf13752
Pontus <pontus00valdes@gmail.com>
2022-05-03 11:14:06 +0200

    heroku

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use(express.static('project'));
 app.use('/posts', postRoute);
-app.use('/weather', weatherRoute);
+app.use(process.env.SECRET_ROUTE, weatherRoute);
 
 app.listen(process.env.PORT || 3000,
     () => console.log("server is running..."));
