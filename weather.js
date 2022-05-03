325fc3482228a58361ec77931488c4d27d45ffa9
Pontus <pontus00valdes@gmail.com>
2022-05-03 11:11:14 +0200

    heroku

--- weather.js
+++ weather.js
@@ -18,21 +18,21 @@ router.get('/', async (req, res) => {
             const results = await response.json();
     
             
-            const reject = () => {
-                res.setHeader('www-authenticate', 'Basic')
-                res.sendStatus(401)
-              }
+            // const reject = () => {
+            //     res.setHeader('www-authenticate', 'Basic')
+            //     res.sendStatus(401)
+            //   }
         
-              const authorization = req.headers.authorization
+            //   const authorization = req.headers.authorization
           
-              if(!authorization) {
-                return reject()
-              }
-              const [username, password] = Buffer.from(authorization.replace('Basic ', ''), 'base64').toString().split(':')
+            //   if(!authorization) {
+            //     return reject()
+            //   }
+            //   const [username, password] = Buffer.from(authorization.replace('Basic ', ''), 'base64').toString().split(':')
           
-              if(!(username === process.env.SECRET_USERNAME && password === process.env.SECRET_PASSWORD)) {
-                return reject()
-              }
+            //   if(!(username === process.env.SECRET_USERNAME && password === process.env.SECRET_PASSWORD)) {
+            //     return reject()
+            //   }
               
             return res.json({
                 success: true,
